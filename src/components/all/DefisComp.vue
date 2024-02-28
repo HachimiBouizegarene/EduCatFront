e44bd73c8b887121cdb8433a7b19ea815a6d2bd4
Mahydine <mlazzouli.pro@gmail.com>
2024-02-28 04:12:15 +0100

    css and responsive for defis component in navbar

--- src/components/all/DefisComp.vue
+++ src/components/all/DefisComp.vue
@@ -1,16 +1,17 @@
 <template>
     <div id="backdrop" @click.self="closeDefis">
         <div id="container">
-            <h1>Defis du Jour !</h1>
+            <h1>Vos Defis !</h1>
             <div v-for="defi in defis" :key="defi.id" class="defi-container">
-                <h3>{{ defi.description }}</h3>
-                <p>Recompense XP: {{ defi.recompenseXP }}</p>
-                <p>Recompense Ecat: {{ defi.recompenseEcat }}</p>
-                <div v-if="defi.infosJeuAssocie">
-                    <h4>Jeu associé: {{ defi.infosJeuAssocie.name }}</h4>
-                    <!-- Vous pouvez afficher l'image ici si nécessaire -->
+                <h3 class="defiTitle">{{ defi.DescriptionDefi }}</h3>
+                <div class="bottom">
+                    <div class="recompenses">
+                        <p>{{ defi.defiData.recompenseXP }} XP <img src="@/assets/images/all/xp-orb.png"></p>
+                        <p>{{ defi.defiData.recompenseECats }} ECAT <img src="@/assets/images/all/ecat-coin.png"></p>
+                    </div>
+                    <router-link :class="{ 'active': defi.defiData.statut === 0 }" class="play-button"
+                        :to="{ name: defi.NomJeu ? defi.NomJeu : 'GamesPage' }">JOUER</router-link>
                 </div>
-                <button :class="{ 'active': defi.statut === 0 }" @click="participerAuDefi(defi.id)">Participer</button>
             </div>
         </div>
     </div>
@@ -36,11 +37,6 @@ export default {
         async fetchAndProcessDefis() {
             await this.$store.dispatch("fetchDefis", { jws: this.$cookies.get('jws'), force: true });
         },
-
-        participerAuDefi(defiId) {
-            // Implémentez la logique pour permettre à l'utilisateur de participer au défi
-            console.log('Participation au défi avec l\'id:', defiId);
-        },
     },
 }
 </script>
@@ -51,26 +47,107 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 500px;
-    height: auto; /* Ajusté pour s'adapter au contenu */
-    background-color: rgb(146, 146, 146);
+    width: 70%;
+    height: auto;
+    /* Ajusté pour s'adapter au contenu */
+    background-color: #202735;
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     padding: 20px;
+    padding-bottom: 40px;
     box-sizing: border-box;
-    overflow-y: auto; /* Pour la scrollbar si nécessaire */
+    overflow-y: auto;
+    /* Pour la scrollbar si nécessaire */
+}
+
+#container h1:first-child {
+    color: white;
+    font-size: clamp(16px, 10vw, 74px);
+    line-height: normal;
+    margin-bottom: 30px;
 }
 
 .defi-container {
-    border: 1px solid #ddd;
+    display: flex;
+    flex-direction: column;
+    border: 2px solid rgb(15, 143, 194);
+    background-color: rgb(0, 187, 255);
+    box-sizing: content-box;
     padding: 10px;
+    width: 90%;
     margin-bottom: 10px;
 }
 
+.defiTitle {
+    font-size: clamp(16px, 2vw, 24px);
+    color: white;
+    margin-bottom: 10px;
+}
+
+.play-button {
+    font-family: 'pixel';
+    text-decoration: none;
+    color: rgb(49, 49, 49);
+    border: none;
+    background: none;
+    font-size: clamp(16px, 1.1vw, 24px);
+    transition: 0.2s ease;
+    padding: 0.4vw 1.5vw;
+    border: 0.2vw solid rgba(255, 187, 0, 0);
+    width: fit-content;
+    height: fit-content;
+}
+
+.recompenses {
+    background-color: #0d445a;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+    padding: 0px 20px;
+    padding-top: 10px;
+    padding-bottom: 3px;
+    width: clamp(15px, 15vw, 190px);
+    box-sizing: content-box;
+}
+
+.recompenses p {
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    font-size: clamp(11px, 1.5vw, 24px);
+    text-align: center;
+    color: white;
+}
+
+.recompenses p img {
+    width: clamp(1px, 4vw, 40px);
+    image-rendering: pixelated;
+}
+
+.bottom {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    width: 100%;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+p,
+a {
+    font-weight: 100;
+    font-family: 'gamesPage', 'Roboto Condensed';
+}
+
 .active {
-    background-color: green;
+    background-color: rgba(8, 241, 36, 0.761);
     color: white;
 }
 
